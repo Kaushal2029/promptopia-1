d8129e39f9caca9d621a098d401da903def5ba21
Mondal Vishal <vishalmondal567@gmail.com>
2024-05-15 15:05:21 +0530

    update profile prompt cards

--- src/components/ProfilePromptCards.jsx
+++ src/components/ProfilePromptCards.jsx
@@ -31,36 +31,35 @@ const ProfilePromptCards = ({ profile }) => {
   }, []);
 
   const filteredPrompts = prompts.length
-    ? prompts.filter((item) => item.user.username === currentUser)
+    ? prompts.filter((item) => item.user.username === profile)
     : [];
 
   return (
     <>
       {filteredPrompts.length ? (
         filteredPrompts.map(
-          (item) =>
-            item.user.username === profile && (
-              <div
-                key={item.id}
-                className="relative grid gap-4 py-4 pr-4 bg-white rounded-md shadow-md"
-              >
-                <p className="px-4 text-sm break-all">
-                  {item.content.length > 87
-                    ? `${item.content.slice(0, 87)} ...`
-                    : item.content}
-                </p>
-                <div className="px-4">
-                  <Chip label={item.title} color="primary" />
-                </div>
-                <div className="absolute right-1 top-3">
-                  {item.user.username === currentUser ? (
-                    <ProfileCardMenu text={item.content} promptId={item.id} />
-                  ) : (
-                    <CopyPrompt />
-                  )}
-                </div>
+          (item) => (
+            <div
+              key={item.id}
+              className="relative grid gap-4 py-4 pr-4 bg-white rounded-md shadow-md"
+            >
+              <p className="px-4 text-sm break-all">
+                {item.content.length > 87
+                  ? `${item.content.slice(0, 87)} ...`
+                  : item.content}
+              </p>
+              <div className="px-4">
+                <Chip label={item.title} color="primary" />
               </div>
-            )
+              <div className="absolute right-1 top-3">
+                {item.user.username === currentUser ? (
+                  <ProfileCardMenu text={item.content} promptId={item.id} />
+                ) : (
+                  <CopyPrompt />
+                )}
+              </div>
+            </div>
+          )
         )
       ) : (
         <h1 className="text-5xl font-bold text-center no-prompts">
